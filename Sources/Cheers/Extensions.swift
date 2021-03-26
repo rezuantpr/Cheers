c3b91b94e23437fabba2af318651f543041ddd8a
rezuantpr <rezuanbidzhiev98@gmail.com>
2021-03-27 00:07:08 +0300

    changes

---
+++ Sources/Cheers/Extensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension Array {
+  /// Creates an array containing all combinations of two arrays.
+  static func createAllCombinations<T, U>(
+    from lhs: Array<T>,
+    and rhs: Array<U>
+  ) -> Array<(T, U)> {
+    let result: [(T, U)] = lhs.reduce([]) { (accum, t) in
+      let innerResult: [(T, U)] = rhs.reduce([]) { (innerAccum, u) in
+        return innerAccum + [(t, u)]
+      }
+      return accum + innerResult
+    }
+    return result
+  }
+}
